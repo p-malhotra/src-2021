d0cc5b74d072b6cb7d591e169a2f10d23ccf6aa3
Pooja Malhotra <er.poojamalhotra@gmail.com>
2021-05-11 23:23:33 -0700

    Update All1.java

--- All1.java
+++ All1.java
@@ -2386,6 +2386,83 @@ public class All1 {
         }
         return res;
     }
+ /**
+     * LeetCode 122
+     * print max profit of stock can buy and sell as many as you want
+     */
+    public int maxProfit(int[] prices) {
+        int i = 0;
+        int val = prices[0];
+        int peak = prices[0];
+        int profit = 0;
+        while (i < prices.length - 1) {
+            while (i < prices.length - 1 && prices[i] >= prices[i + 1])
+                i++;
+            val = prices[i];
+            while (i < prices.length - 1 && prices[i] <= prices[i + 1])
+                i++;
+            peak = prices[i];
+            profit = profit + (peak - val);
 
+        }
+        return profit;
+
+    }
+
+    /**
+     * Leetcode 121 print max profit of stock can buy and sell
+     */
+    public int maxProfitI(int[] prices) {
+        int min = Integer.MAX_VALUE;
+        int maxProfit = 0;
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < min)
+                min = prices[i];
+            int profit = prices[i] - min;
+            if (profit > maxProfit)
+                maxProfit = profit;
+        }
+        return maxProfit;
+
+    }
+    static public int maxProfitForkTransactions(int k, int[] prices) {
+        int n = prices.length;
+        if (n <= 1)
+            return 0;
+
+        //if k >= n/2, then you can make maximum number of transactions.
+        if (k >=  n/2) {
+            int maxPro = 0;
+            for (int i = 1; i < n; i++) {
+                if (prices[i] > prices[i-1])
+                    maxPro += prices[i] - prices[i-1];
+            }
+            return maxPro;
+        }
+
+        int[][] dp = new int[k+1][n];
+        for (int i = 1; i <= k; i++) {
+            int localMax = dp[i-1][0] - prices[0]; //max profit where you already bought the stock on day jj with at most i transactions.
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = Math.max(dp[i][j-1],  prices[j] + localMax);
+                localMax = Math.max(localMax, dp[i-1][j] - prices[j]);
+            }
+        }
+        return dp[k][n-1];
+    }
+    public int maxProfit2Days(int[] prices) {
+        int n=prices.length;
+        int[][] dp = new int[3][n];
+        int localMax = Integer.MIN_VALUE;
+        for (int i = 1; i <= 2; i++) {
+            for (int j = 1; j < n; j++) {
+                localMax = Math.max(localMax, dp[i-1][j-1] - prices[j-1]);
+                dp[i][j] = Math.max(dp[i][j-1], localMax + prices[j]);
+            }
+            localMax = Integer.MIN_VALUE;
+        }
+        return dp[2][n-1];
+
+    }
     }
 
