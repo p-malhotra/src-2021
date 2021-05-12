ee69088d4caf839fd317e574bb1362416af0a95a
Pooja Malhotra <er.poojamalhotra@gmail.com>
2021-05-11 23:05:55 -0700

    Update ListNode.java

--- ListNode.java
+++ ListNode.java
@@ -166,7 +166,7 @@ public class ListNode {
     /**
      *List  has at atmost k nodes, so the operation cost O(n log(K)) where n is the total number of nodes in all the lists
      */
-    public ListNode mergeKLists(ListNode[] lists) {
+    public ListNode mergeKListspq(ListNode[] lists) {
         if(lists==null || lists.length ==0)
             return null;
         PriorityQueue<ListNode> q= new PriorityQueue<ListNode>(lists.length, (a, b)-> a.val-b.val);
@@ -237,5 +237,70 @@ public class ListNode {
             count = count - 1;
         }
         return pre;
+    }
+     /**
+     * O(1) space
+     */
+    public ListNode mergeKListsMergeAll(ListNode[] lists) {
+        int min_index = 0;
+        ListNode head = new ListNode(0);
+        ListNode h = head;
+        while (true) {
+            boolean isBreak = true;
+            int min = Integer.MAX_VALUE;
+            for (int i = 0; i < lists.length; i++) {
+                if (lists[i] != null) {
+                    if (lists[i].val < min) {
+                        min_index = i;
+                        min = lists[i].val;
+                    }
+                    isBreak = false;
+                }
+
+            }
+            if (isBreak) {
+                break;
+            }
+            h.next = lists[min_index];
+            h = h.next;
+            lists[min_index] = lists[min_index].next;
+        }
+        h.next = null;
+        return head.next;
+    }
+     
+      /***
+     * merge by priority queue
+     * time O(nlogk)
+     * Sapce o(n)
+     *
+     */
+    public ListNode mergeKListsPQ(ListNode[] lists) {
+        Comparator<ListNode> cmp;
+        cmp = new Comparator<ListNode>() {
+            @Override
+            public int compare(ListNode o1, ListNode o2) {
+                // TODO Auto-generated method stub
+                return o1.val-o2.val;
+            }
+        };
+//add in queue
+        Queue<ListNode> q = new PriorityQueue<ListNode>(cmp);
+        for(ListNode l : lists){
+            if(l!=null){
+                q.add(l);
+            }
+        }
+        ListNode head = new ListNode(0);
+        ListNode point = head;
+        while(!q.isEmpty()){
+            point.next = q.poll();
+            point = point.next;
+            ListNode next = point.next;
+            if(next!=null){
+                q.add(next);
+            }
+        }
+        return head.next;
     }
     }
